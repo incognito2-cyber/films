af402850f59ba49ae75e2190455770505a5bb95d
incognito2-cyber <genesis6777@mail.ru>
2025-08-02 17:07:55 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,2 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("textcrypt-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "index.html",
-        "manifest.json",
-        "icon.png",
-        "https://cdn.jsdelivr.net/npm/crypto-js@4.1.1/crypto-js.min.js",
-        "https://cdn.jsdelivr.net/npm/qrcode/build/qrcode.min.js",
-        "https://unpkg.com/html5-qrcode"
-      ]);
-    })
-  );
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
-});
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("fetch", () => {});
